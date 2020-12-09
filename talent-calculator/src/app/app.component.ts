e56ded55201055d95a5e27fb2316ce2a04c356bc
Tanya Murrow <tanya.murrow@gmail.com>
2020-12-09 17:52:14 -0600

    Basic mock-up

--- talent-calculator/src/app/app.component.ts
+++ talent-calculator/src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'talent-calculator';
+  totalPoints = 6;
+  currentPoints = this.totalPoints;
+  talentPath = ['stack', 'utensils', 'cake', 'crown', 'ship', 'scuba', 'bolt', 'skull'];
+  talentPathSelected = [false, false, false, false, false, false, false, false];
+
+  ngOnInit() {
+
+  }
+
+  isSelected(rune: string) {
+    return this.talentPathSelected[this.talentPath.indexOf(rune)];
+  }
+
+  select(rune: string) {
+    if (this.currentPoints > 0 && this.talentPathSelected[this.talentPath.indexOf(rune)] == false) {
+        this.talentPathSelected[this.talentPath.indexOf(rune)] = true;
+        this.currentPoints = this.currentPoints - 1;
+    }
+  }
+
+  deselect(rune: string) {
+    this.talentPathSelected[this.talentPath.indexOf(rune)] = false;
+    if (this.currentPoints < this.totalPoints) {
+      this.currentPoints = this.currentPoints + 1;
+    }
+    return false;
+  }
 }
