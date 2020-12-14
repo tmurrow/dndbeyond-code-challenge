802441f444dc41b35136ab9e185b8f4a63658cb7
Tanya Murrow <tanya.murrow@gmail.com>
2020-12-13 22:44:22 -0600

    Update some class names, small style changes, add comments

--- talent-calculator/src/app/app.component.ts
+++ talent-calculator/src/app/app.component.ts
@@ -9,51 +9,73 @@ import { Talent } from './models/talent';
 export class AppComponent {
   title = 'talent-calculator';
   totalPoints = 6;
-  // currentPoints = this.totalPoints;
   spentPoints = 0;
   talentList = [];
   talentPath1 = [];
   talentPath2 = [];
 
   ngOnInit() {
-    if (localStorage.getItem('talentList') !== null) {
-      this.talentList = JSON.parse(localStorage.getItem('talentList'));
-      this.spentPoints = JSON.parse(localStorage.getItem('spentPoints'));
-    } else {
-      this.createTalents();
-    }
+    this.getTalentList();
+    // Separate the main list into a list for each path
     this.talentPath1 = this.talentList.filter(talent => talent.talentPath === 1);
     this.talentPath2 = this.talentList.filter(talent => talent.talentPath === 2);
   }
 
+  // Return if the given talent is selected
   isSelected(talentName: string) {
     return this.getTalent(talentName).selected;
   }
 
+  // Get the talent to select. If there are enough points to buy the talent, if it's not already selected,
+  // and if the previous talent is selected, then select the talent, spend the points, and update the list.
   select(talentName: string) {
     let talent: Talent = this.getTalent(talentName);
     let previousTalent: Talent = this.getTalent(talent.previousTalent);
 
-    if (this.spentPoints < this.totalPoints && !talent.selected && (previousTalent === undefined || previousTalent.selected )) {
+    if (this.spentPoints < this.totalPoints && (this.totalPoints-this.spentPoints) >= talent.cost && !talent.selected && (previousTalent === undefined || previousTalent.selected )) {
       talent.selected = true;
       this.spentPoints += talent.cost;
-      this.setTalent(talent);
+      this.updateTalent(talent);
     }
   }
 
+  // Get the talent to deselect, if it's selected and the next talent in the path isn't selected,
+  // deselect it, update the list, and gain the spent points back.
   deselect(talentName: string) {
     let talent: Talent = this.getTalent(talentName);
     let nextTalent: Talent = this.getTalent(talent.nextTalent);
     if (talent.selected && (nextTalent === undefined || !nextTalent.selected)) {
       talent.selected = false;
-      if (this.spentPoints > 0) {
-        this.spentPoints -= talent.cost;
-      }
-      this.setTalent(talent);
+      this.spentPoints -= talent.cost;
+      this.updateTalent(talent);
     }
     return false;
   }
 
+  // Get a talent from the list by name
+  getTalent(name: string) {
+    return this.talentList.find(r => r.name === name);
+  }
+
+  // Update a talent in the list
+  updateTalent(updatedTalent: Talent) {
+    this.talentList[this.talentList.indexOf(updatedTalent)] = updatedTalent;
+    localStorage.setItem('talentList', JSON.stringify(this.talentList));
+    localStorage.setItem('spentPoints', JSON.stringify(this.spentPoints));
+  }
+
+  // Load the talent list from localStorage if it exists, if not create a new list and set it in localStorage
+  getTalentList() {
+    if (localStorage.getItem('talentList') !== null) {
+      this.talentList = JSON.parse(localStorage.getItem('talentList'));
+      this.spentPoints = JSON.parse(localStorage.getItem('spentPoints'));
+    } else {
+      this.createTalents();
+      localStorage.setItem('talentList', JSON.stringify(this.talentList));
+    }
+  }
+
+  // Create the initial list of talents
   createTalents() {
     this.talentList.push(new Talent('stack', 1, '', 'utensils'));
     this.talentList.push(new Talent('utensils', 1, 'stack', 'cake'));
@@ -64,18 +86,5 @@ export class AppComponent {
     this.talentList.push(new Talent('scuba', 2, 'ship', 'bolt'));
     this.talentList.push(new Talent('bolt', 2, 'scuba', 'skull'));
     this.talentList.push(new Talent('skull', 2, 'bolt', ''));
-    if (localStorage.getItem('talentList') === null) {
-      localStorage.setItem('talentList', JSON.stringify(this.talentList));
-    }
-  }
-
-  getTalent(name: string) {
-    return this.talentList.find(r => r.name === name);
-  }
-
-  setTalent(updatedTalent: Talent) {
-    this.talentList[this.talentList.indexOf(updatedTalent)] = updatedTalent;
-    localStorage.setItem('talentList', JSON.stringify(this.talentList));
-    localStorage.setItem('spentPoints', JSON.stringify(this.spentPoints));
   }
 }
